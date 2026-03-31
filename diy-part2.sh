f582f460c4d358e988d3a392574fb0516c2dafa0
kkoolshear <42595955+kkoolshear@users.noreply.github.com>
2026-03-31 08:32:09 +0800

    Update default login address and password settings

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 #============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Description: OpenWrt DIY script part 2
 #============================================================
 
-# 修改openwrt默认登陆地址192.168.1.1改成192.168.18.1
-sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
+# 1. 修改默认登录地址为 192.168.1.2 (避开光猫 1.1)
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-#设置登录密码为空
+# 2. 设置登录密码为空 (Lean 源码专用)
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
